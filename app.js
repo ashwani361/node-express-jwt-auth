920d7929e13ea74bbd87f58151c776f71069c175
Ashwani Kumar <62863003+ashwani361@users.noreply.github.com>
2023-06-08 21:20:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://<name>:<password>@cluster0.del96.mongodb.net/node-auth';
+const dbURI = '';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
